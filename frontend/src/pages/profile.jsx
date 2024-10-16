8e93b5a0e3eb0515b27254f5ea7260d2efa38e86
TinBaloyi <baloyitintswalo92@gmail.com>
2024-10-16 16:16:03 +0200

    update

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -104,7 +104,7 @@ const ProfilePage = () => {
     }
 
     try {
-      const response = await fetch(`https://hmsstackmasters-hvfcb8drb4d0egf8.southafricanorth-01.azurewebsites.net//users/update/${userId}`, {
+      const response = await fetch(`https://hmsstackmasters-hvfcb8drb4d0egf8.southafricanorth-01.azurewebsites.net/users/update/${userId}`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
